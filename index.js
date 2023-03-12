8799a05405f90b459b19461b75564f0dd229a098
nicole <nicolenamayan133@gmail.com>
2023-03-12 11:57:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const Cards = (["Ada", "Brendan", "Ali",],"birthday")
+
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you ${cards[i]} for the wonderful birthday gift!`);
+    debugger;
+  }
+
+  return cards;
+}
+
+writeCards(Cards);
